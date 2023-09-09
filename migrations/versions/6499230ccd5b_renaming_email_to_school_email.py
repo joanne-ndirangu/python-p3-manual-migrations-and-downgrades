b662eb3376832ed03f0962f9aec1eee130ac2b82
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-09-09 22:35:39 +0300

    Completed assignment

---
+++ migrations/versions/6499230ccd5b_renaming_email_to_school_email.py
@@ -0,0 +1,24 @@
+"""Renaming email to school_email
+
+Revision ID: 6499230ccd5b
+Revises: 
+Create Date: 2023-09-09 22:34:22.194194
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6499230ccd5b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
